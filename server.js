12c68f3506b41d14b623d6d48312de0d2b7d1a68
Marcin Kaleta <kalet1984@gmail.com>
2019-09-05 21:13:46 +0200

    Done middleware implementation -module 25.5

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 var express = require('express');
 var app = express();
-app.use(express.static('assets'));
+
+app.use('/store', function(req, res, next){
+    console.log('Hej, jestem pośrednikiem przy żądaniu do /store');
+    next();
+});
+
 
 app.get('/', function (req, res) {
-    res.send('/index.html');
+    res.send('Hello World!');
 });
 
-app.get('/userform', function (req, res) {
-    const response = {
-        first_name: req.query.first_name,
-        last_name: req.query.last_name
-    };
-    res.json(response);
+
+app.get('/store', function(req, res){
+    res.send('To jest sklep');
 });
 
-var server = app.listen(3000, 'localhost', function() {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
-});
-
+app.listen(3000)
