98231e5ac0df55e0c68cc9f9a9b5ddceaf574cd3
Marcin Kaleta <kalet1984@gmail.com>
2019-09-06 18:18:48 +0200

    Done configure login app - 25.6-PugTemplates

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 var express = require('express');
 var app = express();
 
-app.use('/store', function(req, res, next){
-    console.log('Hej, jestem pośrednikiem przy żądaniu do /store');
-    next();
+// Pug declarations
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.get('/', function(req, res){
+    res.render('login', {
+        title: "Dummy login app",
+        url: "/auth/google",
+    });
+});
+
+app.get('/auth/google', function (req, res) {
+    res.render('logged', {
+        user: {
+            first_name: req.query.first_name,
+            last_name: req.query.last_name
+        }
+    });
 });
 
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
-
-app.get('/store', function(req, res){
-    res.send('To jest sklep');
-});
 
 app.listen(3000)
